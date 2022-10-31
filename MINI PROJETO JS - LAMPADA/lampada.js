824bd7b131daf7ed5d240700ae0e3e734bf5a0ea
JottaEnnie <jncontatojn@gmail.com>
2022-10-31 16:40:24 -0300

    remocao de um dos botoes - desafio concluido e mini projeto finalizado

--- MINI PROJETO JS - LAMPADA/lampada.js
+++ MINI PROJETO JS - LAMPADA/lampada.js
@@ -1,30 +1,49 @@
-const turnOn = document.getElementById('turnOn');
-const turnOff = document.getElementById('turnOff');
-const lamp = document.getElementById('lamp');
+const turnOnOff = document.getElementById( 'turnOnOff' );
+const lamp = document.getElementById( 'lamp' );
 
-
-function isLampBroken() {
+function isLampBroken (){
     return lamp.src.indexOf('quebrada') > -1
 }
 
-function lampOn() {
-    if (!isLampBroken()) {
+function lampOn (){
+    if (!isLampBroken()){
         lamp.src = './img/ligada.jpg';
+        turnOnOff.textContent = 'Desligar';
+    }
+}
+function lampOff (){
+    if (!isLampBroken()){
+        lamp.src = './img/desligada.jpg';
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+function lampBroken (){
+    if (!isLampBroken()){
+        lamp.src = './img/quebrada.jpg';
+        turnOnOff.src = lampOnOff();
+    }
+}
+function lampOnOff (){
+
+    if (turnOnOff.textContent == 'Ligar' && !isLampBroken()){
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
     }
 }
 
-function lampOff() {
-    if (!isLampBroken()) {
-    lamp.src = './img/desligada.jpg';
-}
-}
-
-function lampBroken() {
-    lamp.src = './img/quebrada.jpg';
+function lampBrokenReset(){
+    if(isLampBroken()){
+        lamp.src = './img/desligada.jpg';
+        lampOff();
+    }
 }
 
-turnOn.addEventListener('click', lampOn);
-turnOff.addEventListener('click', lampOff);
-lamp.addEventListener('mouseover', lampOn);
-lamp.addEventListener('mouseleave', lampOff)
+
+turnOnOff.addEventListener('click', lampOnOff);
 lamp.addEventListener('dblclick', lampBroken);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+turnOnOff.addEventListener('dblclick', lampBrokenReset);
